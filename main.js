97ab15cc2f56b150cc078a2a5d4988b7a36912a1
bh0 <bryce13h0@gmail.com>
2019-05-09 08:31:51 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+    let terms = [
+        {term: "national speed limit", definition: "lorem"}, 
+        {term: "give way", definition: "lorem2" },
+        {term: "overtaking", definition: "lorem3"}, 
+        {term: "clearway", definition: "lorem4"},
+        {term: "dual carriage way", definition: "lorem bada ba bada ba ba ba dskasnkldandkldnksdnsklndklasndskasnkldandkldnksdnsklndklasndskasnkldandkldnksdnsklndklasndskasnkldandkldnksdnsklndklasn5" }
+    ];
+
+    terms.forEach(term => { 
+        document.querySelector(".terms").innerHTML += `<li class="term tooltip">
+            <b>${term.term}
+                <span class="tooltip-bottom">${term.definition}</span>
+            </b></li>`;
+    }); 
+
