95bf859f40ccf5e4e02daf436afa2a53807114c5
bh0 <bryce13h0@gmail.com>
2019-05-21 10:24:03 +0100

    replace 'var' with 'let'

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -12,11 +12,15 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("road-signs");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
+  let i;
+  let slides = document.getElementsByClassName("road-signs");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  } 
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
