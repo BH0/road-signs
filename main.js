4f24f4a32efdeba502bd416644bd77a4fe68fe31
bh0 <bryce13h0@gmail.com>
2019-05-22 02:04:41 +0100

    User can search for term by defintiion or the term itself

--- main.js
+++ main.js
@@ -7,10 +7,50 @@
         {term: "dual carriage way", definition: "lorem bada ba bada ba ba ba dskasnkldandkldnksdnsklndklasndskasnkldandkldnksdnsklndklasndskasnkldandkldnksdnsklndklasndskasnkldandkldnksdnsklndklasn5" }
     ];
 
-    terms.forEach(term => { 
-        document.querySelector(".terms").innerHTML += `<li class="term tooltip">
-            <b>${term.term}
-                <span class="tooltip-bottom">${term.definition}</span>
-            </b></li>`;
-    }); 
+  
+terms.forEach(term => { 
+    document.querySelector(".terms").innerHTML += `<li class="term tooltip">
+        <b>${term.term}
+            <span class="tooltip-bottom">${term.definition}</span>
+        </b></li>`;
+}); 
 
+document.querySelector("#search-term").addEventListener("keyup", e => { 
+    document.querySelector(".terms").innerHTML = ""; 
+    if (document.querySelector("#search-term").value != "") { 
+        let termsToRender = terms.filter(term => term.term.toLowerCase().includes(e.target.value)); 
+        termsToRender.forEach(term => { 
+            document.querySelector(".terms").innerHTML += `<li class="term tooltip">
+                <b>${term.term}
+                    <span class="tooltip-bottom">${term.definition}</span>
+                </b></li>`;
+        }); 
+    } else { 
+        terms.forEach(term => { 
+            document.querySelector(".terms").innerHTML += `<li class="term tooltip">
+                <b>${term.term}
+                    <span class="tooltip-bottom">${term.definition}</span>
+                </b></li>`;
+        }); 
+    }
+}); 
+
+document.querySelector("#search-definition").addEventListener("keyup", e => { 
+    document.querySelector(".terms").innerHTML = ""; 
+    if (document.querySelector("#search-definition").value != "") { 
+        let termsToRender = terms.filter(term => term.definition.toLowerCase().includes(e.target.value)); 
+        termsToRender.forEach(term => { 
+            document.querySelector(".terms").innerHTML += `<li class="term tooltip">
+                <b>${term.term}
+                    <span class="tooltip-bottom">${term.definition}</span>
+                </b></li>`;
+        }); 
+    } else { 
+        terms.forEach(term => { 
+            document.querySelector(".terms").innerHTML += `<li class="term tooltip">
+                <b>${term.term}
+                    <span class="tooltip-bottom">${term.definition}</span>
+                </b></li>`;
+        }); 
+    }
+}); 
